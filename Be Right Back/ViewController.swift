0c012da60858ca6bc3fd1c1ed069925fe7ffe3fd
Gilang Sinawang <giirugiru@gmail.com>
2019-09-17 12:22:46 +0700

    Initial Commit

---
+++ Be Right Back/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Be Right Back
+//
+//  Created by Gilang Sinawang on 17/09/19.
+//  Copyright © 2019 Gilang Sinawang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
